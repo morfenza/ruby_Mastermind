5d753ff9aeb7f6d06a2e1e206f424be52a8c8790
Morfenza <ianlourenco@protonmail.com>
2023-02-02 17:46:09 -0300

    Create codebreaker class basic structure

---
+++ lib/codebreaker.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Class defining the Codebreaker
+class Codebreaker
+  attr_accessor :guess
+
+  def guess_code(guess)
+    @guess = guess.split('').map(&:to_i)
+  end
+end
