508962c0d28202290be1a31ec8a3fd06c97c7bcc
Morfenza <ianlourenco@protonmail.com>
2023-02-03 13:45:11 -0300

    Organize modules

---
+++ lib/modules/check.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Module to help check conditions
+module Check
+  def ask_for_guess(round_num)
+    puts "##{round_num}: Please write four numbers from 1 to 6 (1..6)"
+
+    while true
+      guess = gets.chomp
+      break if valid_guess?(guess)
+
+      puts 'Please, write a valid guess'
+    end
+
+    guess
+  end
+
+  def valid_guess?(guess)
+    return false if guess.size > 4 || guess.size <= 0
+
+    guess.split('').each do |num|
+      return false if num.to_i > 6 || num.to_i < 1
+    end
+
+    true
+  end
+
+  def won?(code, guess)
+    guess.eql?(code)
+  end
+end
