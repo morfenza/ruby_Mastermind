a53d45abce443e527eb19a56c1f85857f13357f6
Morfenza <ianlourenco@protonmail.com>
2023-02-03 17:55:26 -0300

    Create option 2 game loop

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 
-require './base/game'
+require_relative 'game_option1'
+require_relative 'game_option2'
+require_relative 'check'
+require_relative 'messages'
 
-def play_game; end
+# rubocop:disable Style/MixinUsage
+include Messages
+# rubocop:enable Style/MixinUsage
 
-def repeat_game; end
+# Option 1: Human Codebreaker vs Machine Codemaker
+# Option 2: Machine Codebreaker vs Human Codemaker
+
+def game_loop
+  loop do
+    welcome_message
+    if Check.ask_for_game_modes == 1
+      #tba
+    else
+      game = GameOption1.new
+      game.create_game
+      game.play_match
+    end
+
+    break unless Check.play_again?
+  end
+end
+
+game_loop
