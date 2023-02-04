766b5bd5a9499c598eb86338c5ce6251043112b4
Morfenza <ianlourenco@protonmail.com>
2023-02-04 07:12:28 -0300

    Fix code type and super method call

--- lib/human_codemaker.rb
+++ lib/human_codemaker.rb
@@ -5,7 +5,7 @@ require_relative 'codemaker'
 # Class defining the human codemaker
 class HumanCodemaker < Codemaker
   def create_code(code)
-    @code = code
-    super
+    @code = code.split('').map(&:to_i)
+    super(@code)
   end
 end
