fcd4293150ce8cbf87c523cc8788a990846bc79c
Morfenza <ianlourenco@protonmail.com>
2023-02-06 15:23:47 -0300

    Implement a better 'AI' and correct bugs

--- lib/players/machine_codebreaker.rb
+++ lib/players/machine_codebreaker.rb
@@ -4,17 +4,50 @@ require_relative '../base/codebreaker'
 
 # Class defining the machine codebreaker
 class MachineCodebreaker < Codebreaker
-  attr_accessor :cur_num
-
   # rubocop:disable Lint/MissingSuper
   def initialize
-    self.cur_num = 1
-    self.guess = [1, 1, 1, 1]
+    @digit = 1
+    @guess = Array.new(4, @digit)
+    @ans = []
+    @permutated = false
+    @all_combinations = []
   end
   # rubocop:enable Lint/MissingSuper
 
-  def guess_code(round_num, clues)
+  def guess_code(_round_num, clues)
+    # If we know all 4 numbers, we will find all combinations and try them
+    return try_possibilities(clues) if @ans.size == 4
+
+    # While we don't know the 4 numbers, we keep checking
+    #
+    #   - there is need for repetition of the guard clause
+    #     because of the way the first guess works
+    #
+    #     - this prevents a bug where on round is wasted!
+    check_feedback(clues)
+    return try_possibilities(clues) if @ans.size == 4
+
     clues.clear
-    self.guess = Array.new(4) { rand(1..6) }
+    @digit += 1
+    @guess = Array.new(4, @digit)
+  end
+
+  def permute_ans
+    @all_combinations = []
+    @ans.permutation(4) { |permutation| @all_combinations << permutation }
+    @all_combinations = @all_combinations.uniq
+    @permutated = true
+  end
+
+  def try_possibilities(clues)
+    permute_ans if @permutated == false
+    clues.clear
+    @guess = @all_combinations.delete(@all_combinations.sample)
+  end
+
+  def check_feedback(clues)
+    return if clues.empty?
+
+    clues.size.times { @ans << @digit }
   end
 end
